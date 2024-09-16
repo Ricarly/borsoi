4e5b680c2d2bf10be086c8b5157c731199dc4581
Ricarly <ricarlysantos@gmail.com>
2024-09-16 12:17:10 -0300

    comitado10

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
+import sys
+import os
+
+print("Python version:", sys.version)
+print("Current working directory:", os.getcwd())
+print("Contents of current directory:", os.listdir())
+
 from main import app
 
 if __name__ == "__main__":
+    print("Starting the application...")
     app.run()
